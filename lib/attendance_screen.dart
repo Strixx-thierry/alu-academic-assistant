94e9eb7bb11d62499c30ee2547f19499713f49ba
Tedla Tesfaye <t.godebo@alustudent.com>
2026-02-05 04:24:48 +0200

    Add session state and sample attendance data

--- lib/attendance_screen.dart
+++ lib/attendance_screen.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
+import 'models.dart';
 
-class AttendanceScreen extends StatelessWidget {
-  const AttendanceScreen({super.key});
+class AttendanceScreen extends StatefulWidget {
+  const AttendanceScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AttendanceScreen> createState() => _AttendanceScreenState();
+}
+
+class _AttendanceScreenState extends State<AttendanceScreen> {
+
+  List<Session> _sessions = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadSampleData();
+  }
+
+  void _loadSampleData() {
+    _sessions = [
+      Session(
+        title: "Math Class",
+        date: DateTime.now(),
+        startTime: "10:00",
+        endTime: "12:00",
+        location: "Room A",
+        type: "Class",
+        isPresent: true,
+      ),
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Attendance Screen')));
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Attendance"),
+      ),
+      body: Center(
+        child: Text("Sessions: ${_sessions.length}"),
+      ),
+    );
   }
 }
+
